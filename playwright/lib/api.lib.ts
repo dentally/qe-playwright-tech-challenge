fbca3c82407aadbf3e10b9bcd6ca7990f329ef9f
Mark Jones <robert.jones@dentally.co>
2025-12-01 13:39:27 +0000

    initial draft converted to playwright

---
+++ playwright/lib/api.lib.ts
@@ -0,0 +1,44 @@
+import { Page } from '@playwright/test';
+
+interface ApiBody {
+  operationName: string;
+  variables: {
+    slug: string;
+    title: string;
+  };
+  query: string;
+}
+
+class _Api {
+  constructor(private page: Page) {}
+
+  async getUuid(): Promise<string> {
+    // get the uuid ready for the network request
+    const url = this.page.url();
+    const uuid = url.split('/').pop() || '';
+    return uuid;
+  }
+
+  apiBody(uuid: string): ApiBody {
+    return {
+      operationName: 'createWorksItem',
+      variables: {
+        slug: uuid,
+        title: 'hey I sent this via a POST request!',
+      },
+      query:
+        'mutation createWorksItem($slug: String!, $title: String!) {\n  createWorksItem(retroSlug: $slug, title: $title) {\n    id\n    hidden\n    title\n    userUuid\n    votes\n    __typename\n  }\n}\n',
+    };
+  }
+
+  async createItem() {
+    const uuid = await this.getUuid();
+    const body = this.apiBody(uuid);
+
+    await this.page.request.post('https://www.retrotool.app/api/graph', {
+      data: body,
+    });
+  }
+}
+
+export const createApi = (page: Page) => new _Api(page);
---
+++ tests/1_e2e.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+import { createRetro, ticketTypes } from '../playwright/lib/retro.lib';
+import boardItems from '../playwright/fixtures/items.json';
+import boardVotes from '../playwright/fixtures/votes.json';
+
+test.describe('Retro', () => {
+  test.beforeEach(async ({ page }) => {
+    const retro = createRetro(page);
+    await retro.visit();
+
+    await page.locator('[href="/new"]').click();
+    // wait for page to load
+    await expect(page.locator('[class*="Header__HeaderContainer"]')).toBeVisible();
+    await page.waitForTimeout(1000);
+  });
+
+  test.only('Set a board password', async ({ page }) => {
+    const retro = createRetro(page);
+    await retro.setPassword('1234');
+    await retro.removePassword('1234');
+  });
+
+  test('Complete a retro', async ({ page }) => {
+    const retro = createRetro(page);
+
+    for (const item of boardItems.good) {
+      await retro.addTicket(ticketTypes.GOOD, item);
+    }
+
+    for (const item of boardItems.bad) {
+      await retro.addTicket(ticketTypes.BAD, item);
+    }
+
+    for (const item of boardItems.questions) {
+      await retro.addTicket(ticketTypes.QUESTIONS, item);
+    }
+
+    await retro.setToVote();
+    await retro.setToActions();
+
+    // to do
+    // add action items found in boardItems.actions
+    // assert the action item are listed
+    // finish the retro and assert the retro is no longer active
+  });
+});
